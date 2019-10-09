f97cb0f42749cab43c74333807f03dbe4c6762b5
Pedro Marinho <pedro.marinho238@gmail.com>
2019-10-08 21:37:31 -0400

    argumentos

--- snap/local/photogimp.sh
+++ snap/local/photogimp.sh
@@ -10,10 +10,10 @@ echo $FILE
 
 if [ -e "$DIR$FILE" ] ; then
 #     echo "o arquivo photogimp-teste existe"
-    $SNAP/usr/bin/gimp
+    $SNAP/usr/bin/gimp $* 
 else
 #     echo "o arquivo photogimp-teste não existe"
     mkdir -p $SNAP_USER_DATA/.config/GIMP/2.10
     tar xfv $SNAP/photogimp.tar -C "$DIR"
-    $SNAP/usr/bin/gimp
+    $SNAP/usr/bin/gimp $* 
 fi
