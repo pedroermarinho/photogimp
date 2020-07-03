50bbb1dc07eb6f968945986a6666cf6400c09a05
Pedro Marinho <pedro.marinho238@gmail.com>
2020-07-03 00:43:39 -0400

    2.10.20

--- snap/local/photogimp.sh
+++ snap/local/photogimp.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 DIR=$SNAP_USER_DATA/.config/GIMP/2.10/
-FILE="photogimp-teste1"
+FILE="photogimp"
 echo $FILE
-# ech$SNAP_USER_DATA
-# echo "o "SNAP_USER_DATA"
-# echo SNAP"
-# echo $SNAP
-#/home/zyga/snap/hello-world/27
 
 if [ -e "$DIR$FILE" ] ; then
-#     echo "o arquivo photogimp-teste existe"
     $SNAP/usr/bin/gimp $* 
 else
-#     echo "o arquivo photogimp-teste não existe"
-    mkdir -p $SNAP_USER_DATA/.config/GIMP/2.10
-    tar xfv $SNAP/photogimp.tar -C "$DIR"
+    $SNAP/usr/bin/gimp -i &
+    sleep 5
+    kill $!
+    cp -r $SNAP/config/* $DIR
     $SNAP/usr/bin/gimp $* 
 fi
